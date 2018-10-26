ff7e8d5669e3d27dde6901bdaa888f94d114d646
Kranti Chavannavar <kranthic@a10networks.com>
2018-10-26 16:43:52 +0530

    index

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const app = require('./config/express');
 require('./config/mongoose');
 
 // module.parent check is required to support mocha watch
+// src: https://github.com/mochajs/mocha/issues/1912
 if (!module.parent) {
   app.listen(config.port, () => {
     console.info(`server started on port ${config.port} (${config.env})`);
