05d0d019b8f3cca8bf3f5ed1ab57e7f2738b26f2
Kranti Chavannavar <kranti.chavannavar@publicissapient.com>
2021-03-19 02:55:05 +0530

    Speech Recognition Service improvements

--- src/app/components/speech-interpreter/speech-interpreter.service.ts
+++ src/app/components/speech-interpreter/speech-interpreter.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { environment } from '@env/environment';
 import { HttpClient } from '@angular/common/http';
+import * as feedings from '../../../assets/data/feedings.json';
 
 @Injectable({
   providedIn: 'root'
@@ -14,10 +15,7 @@ export class SpeechInterpreterService {
   }
 
   loadFeedings() {
-    const jsonURL = environment.endPoint + '/data';
-    this.http.get(jsonURL).subscribe((resp: any) => {
-      this.data = resp;
-    });
+    this.data = feedings;
   }
 
   interpretSpeech(speechText: string) {
@@ -25,19 +23,59 @@ export class SpeechInterpreterService {
     if (!this.data) {
       this.loadFeedings();
     }
-    speechText = 'get repos from June 19th to 20th';
+    speechText = 'get repos from June 19th';
 
     const speechMappings = this.data.speechMappings;
 
     let mappedActions = [];
-    speechMappings.forEach(wordMap => {
-      wordMap.keys.forEach((key) => {
-        if (speechText.indexOf(key) > -1) {
-          mappedActions.push(wordMap.action);
-          speechText = speechText.replace(key, '');
-        }
+    // speechMappings.forEach(wordMap => {
+    //   wordMap.keys.forEach((key) => {
+
+    //     let keys = [];
+    //     if (key.indexOf('+') > -1) {
+    //       keys = key.split('+');
+
+    //       keys.forEach((key) => {
+    //         const speechTextMatchIndex = speechText.indexOf(key);
+    //         if (speechTextMatchIndex > -1) {
+    //           mappedActions.push(wordMap.action);
+    //           if (wordMap.action === 'DATE_RANGE') {
+    //             mappedActions.push(speechText.substr(speechTextMatchIndex));
+    //           }
+    //           speechText = speechText.replace(key, '');
+    //         }
+    //       });
+
+    //     } else {
+    //       const speechTextMatchIndex = speechText.indexOf(key);
+    //         if (speechTextMatchIndex > -1) {
+    //           mappedActions.push(wordMap.action);
+    //           if (wordMap.action === 'DATE_RANGE') {
+    //             mappedActions.push(speechText.substr(speechTextMatchIndex));
+    //           }
+    //           speechText = speechText.replace(key, '');
+    //         }
+    //     }
+    //   });
+    // });
+
+
+    let speechTextArr = speechText.split(' ');
+
+    speechTextArr.map((speechTextWord: string) => {
+      
+
+      const mappedObj = speechMappings.find((speechMapping: any) => {
+        return speechMapping.keys.includes(speechTextWord);
       });
-    });
+
+      if (mappedObj) {
+        mappedActions.push(mappedObj.action);
+      } else {
+        mappedActions.push(speechTextWord);
+      }
+    })
+
 
     console.log(mappedActions);
   }
